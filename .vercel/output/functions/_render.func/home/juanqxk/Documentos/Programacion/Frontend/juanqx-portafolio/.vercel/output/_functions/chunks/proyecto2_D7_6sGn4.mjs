72eccf7b2f40b46386c3464eb1d5e97238e99773
Juanqx <Antony556_04@outlook.com>
2024-06-04 23:45:00 -0600

    cambios de deploy

--- .vercel/output/functions/_render.func/home/juanqxk/Documentos/Programacion/Frontend/juanqx-portafolio/.vercel/output/_functions/chunks/proyecto2_D7_6sGn4.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/proyecto2_DlYS_PKP.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/home/juanqxk/Documentos/Programacion/Frontend/juanqx-portafolio/.vercel/output/_functions/chunks/proyecto3_DkxvxcMr.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/proyecto3_Cjj-0UVs.mjs');
-
-export { page };
